afe44042894f60162213490128d083fa7f206ead
Raphael Cohn <raphael.cohn@stormmq.com>
2017-01-15 12:50:11 +0000

    Initial commit

---
+++ Document.lua
@@ -0,0 +1,13 @@
+--[[
+This file is part of toml. It is subject to the licence terms in the COPYRIGHT file found in the top-level directory of this distribution and at https://raw.githubusercontent.com/pallene-modules/toml/master/COPYRIGHT. No part of toml, including this file, may be copied, modified, propagated, or distributed except according to the terms contained in the COPYRIGHT file.
+Copyright © 2015 The developers of toml. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/pallene-modules/toml/master/COPYRIGHT.
+]]--
+
+
+local halimede = require('halimede')
+local toml = require('toml')
+local TOML = toml.TOML
+local Document = halimede.moduleclass('Document')
+
+function module:initialize()
+end
